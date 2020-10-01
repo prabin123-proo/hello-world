8e5b27bcbd404d7b2968787a39a2c60346f03d7f
prabin123-proo <71684870+prabin123-proo@users.noreply.github.com>
2020-10-01 20:59:27 +0545

    creative hello world programme
    using  creativity in programme  makes you good prograammer

--- hello.py
+++ hello.py
@@ -5,3 +5,16 @@ def something(x):
 
 print("hello world")
 print(something(2))
+
+def creative_helloworld_program():
+	print("hh       hh   eeeeeeeeee  l                   l                oooooooooooooo      W          ")
+	print("hh       hh   eeeeeeee    ll                 ll                oo          oo      O ")
+	print("hh       hh   ee          lll               lll                oo          oo      R")
+	print("hh       hh   eeeeee      lll               lll                oo          oo      L") 
+	print("hh       hh   eeeeee      lll               lll                oo          oo      D")
+	print("hhhhhhhhhhh   ee          lll               lll                oo          oo      :)
+	print("hh       hh   eeeeeee     lllllllllllllll   llllllllllllll      ooooooooooooo                                       ")
+	print("hh       hh   eeeeeeeee   lllllllllllll     llllllllllll                                              ")
+
+    
+
